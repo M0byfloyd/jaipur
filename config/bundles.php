240ca4e8f552631172e3436286b52e281f992ea9
M0byfloyd <alexandre.robert2810@gmail.com>
2020-10-31 01:21:32 +0100

    Ouaais on a résolu le problème

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
